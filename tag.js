aa727baedf7274a0af9aa382bca6dca1545d7936
Dollbug <1227009721@qq.com>
2019-07-01 10:09:47 +0800

    release.v1.1906.54

--- tag.js
+++ tag.js
@@ -8,23 +8,21 @@ const tag = currentBranch.slice(
   currentBranch.length
 );
 console.log("当前分支：", currentBranch);
+
+// 提交本地
 if (currentBranch !== "master") {
   execSync("git add .");
-  execSync(`git commit -m ${currentBranch}发版`);
-  execSync(`git pull origin ${currentBranch}`);
-  execSync(`git push origin ${currentBranch}`);
+  execSync("git commit -m " + currentBranch);
+  execSync("git pull origin " + currentBranch);
+  execSync("git push origin " + currentBranch);
   execSync("git checkout master");
 }
 
 execSync("git pull origin master");
-execSync(`git merge ${currentBranch}`);
-execSync(`git merge ${currentBranch}`);
-execSync("git add .");
-execSync(`git commit -m merge:${currentBranch}`);
-execSync(`git pull origin master`);
-execSync(`git push origin master`);
+// 使用gitland merge
+// execSync(`git merge ${currentBranch}`);
 
-execSync(`git tag ${tag}`);
+execSync("git tag " + tag);
 execSync("git push origin --tags");
-execSync(`git push origin :${currentBranch}`);
-execSync(`git branch ${currentBranch} -D`);
+execSync("git push origin :" + currentBranch);
+execSync("git branch " + currentBranch + " -D");
